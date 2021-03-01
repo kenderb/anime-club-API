8948ca0b10c4e7c9888a844f7e8edc17bfb3b420
kenderb <kenderbolivar@gmail.com>
2021-03-01 12:37:29 -0500

    Add the registrations controller

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,7 @@
 class StaticController < ApplicationController
+
   def home
     render json: {status: "ok"}
   end
-  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :sessions, only: [:create]
+  resources :registrations, only: [:create]
   root to: "static#home"
 end
