4f7c17493d3eef7fc475bc7744467deabfedeec6
kenderb <kenderbolivar@gmail.com>
2021-03-01 14:05:13 -0500

    Add the heroku domain

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production"
-  Rails.application.config.session_store :cookie_store, key: "_anime_club_app", domain: "Domain for the API APP"
+  Rails.application.config.session_store :cookie_store, key: "_anime_club_app", domain: "anime-rails-api.herokuapp.com"
 else
   Rails.application.config.session_store :cookie_store, key: "_anime_club_app"
 end
