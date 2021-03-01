ed67c3f0e20f7d660cf574d6b01059bc41765667
kenderb <kenderbolivar@gmail.com>
2021-03-01 14:17:03 -0500

    Add a empty space on sesion_store.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,4 +2,5 @@ if Rails.env == "production"
   Rails.application.config.session_store :cookie_store, key: "_anime_club_app", domain: "anime-rails-api.herokuapp.com"
 else
   Rails.application.config.session_store :cookie_store, key: "_anime_club_app"
+  
 end
