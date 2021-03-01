e7734d02951f6c8eb7d5f033050a8326ad136159
kenderb <kenderbolivar@gmail.com>
2021-03-01 14:01:39 -0500

    Add some production conditional for change the domain

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,5 @@
-Rails.application.config.session_store :cookie_store, key: "_anime_club_app", domain: "Domain for the API APP"
+if Rails.env == "production"
+  Rails.application.config.session_store :cookie_store, key: "_anime_club_app", domain: "Domain for the API APP"
+else
+  Rails.application.config.session_store :cookie_store, key: "_anime_club_app"
+end
