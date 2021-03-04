7e48baf69d1d9537c9e96a74cbf7fbcf673d4f86
kenderb <kenderbolivar@gmail.com>
2021-03-03 20:34:05 -0500

    Add domain anime-rails-api.herokuapp.com

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,11 +1,11 @@
 if Rails.env == "production"
   Rails.application.config.session_store :cookie_store, {
     :key => '_anime_club_app',
-    :domain => :all,
+    :domain => "anime-rails-api.herokuapp.com",
     :same_site => :none,
     :secure => :true,
     :tld_length => 2
-  }, domain: "anime-rails-api.herokuapp.com"
+  }
 else
   Rails.application.config.session_store :cookie_store, {
     :key => '_anime_club_app',
