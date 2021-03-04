d55521d821ffb7b4140a833dc6bd38926b1a83bb
kenderb <kenderbolivar@gmail.com>
2021-03-03 20:05:15 -0500

    Set the same site to the app sessions

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,18 @@
 if Rails.env == "production"
-  Rails.application.config.session_store :cookie_store, key: "_anime_club_app", domain: "anime-rails-api.herokuapp.com"
+  Rails.application.config.session_store :cookie_store, {
+    :key => '_anime_club_app',
+    :domain => :all,
+    :same_site => :none,
+    :secure => :true,
+    :tld_length => 2
+  }, domain: "anime-rails-api.herokuapp.com"
 else
-  Rails.application.config.session_store :cookie_store, key: "_anime_club_app"
+  Rails.application.config.session_store :cookie_store, {
+    :key => '_anime_club_app',
+    :domain => :all,
+    :same_site => :none,
+    :secure => :true,
+    :tld_length => 2
+  }
   
 end
