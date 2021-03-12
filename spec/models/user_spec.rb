83fb2300f6305e716357c5b9e2e6271eed17bd0c
kenderb <kenderbolivar@gmail.com>
2021-03-12 10:52:23 -0500

    Fix typo

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe User, type: :model do
       user.save
       expect(user.errors.details).to eq({:email=>[{:error=>:invalid, :value=>"user one name"}]})
     end
-    it 'accpet a correct email' do
+    it 'accept a correct email' do
       user = User.new(name: 'userone', email: 'kender@mail.com', password: '123456', password_confirmation: '123456')
       expect(user.save).to be(true)
     end
