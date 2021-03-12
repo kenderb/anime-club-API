e47d2a0348de09296fa5c9360cf91e59813dc914
kenderb <kenderbolivar@gmail.com>
2021-03-12 11:21:38 -0500

    Fix linter erros on the anime_controller_spec

--- spec/controllers/anime_controller_spec.rb
+++ spec/controllers/anime_controller_spec.rb
@@ -1,8 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe AnimesController, type: :controller do
-
-  let!(:anime) { Anime.create!(title: 'anime 1',  url_image: 'some url', description: 'some text') }
+  let!(:anime) { Anime.create!(title: 'anime 1', url_image: 'some url', description: 'some text') }
 
   describe 'GET /animes' do
     before do
@@ -14,19 +13,17 @@ RSpec.describe AnimesController, type: :controller do
         expect(response).to have_http_status(200)
       end
     end
-
   end
-  
+
   describe 'GET /animes/:id' do
     before do
       anime
-      get :show, params: {id: anime.id}
+      get :show, params: { id: anime.id }
     end
     context 'when there are animes' do
       it 'returns status code 200' do
         expect(response).to have_http_status(200)
       end
     end
-
   end
 end
