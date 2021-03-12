0db8a180fe1cff7b8ba1e3f5862791c53108f4c0
kenderb <kenderbolivar@gmail.com>
2021-03-12 15:03:54 -0500

    Add the registrations controller spec

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,13 +1,13 @@
 class RegistrationsController < ApplicationController
   def create
-    user = User.create!(
+    user = User.new(
       name: params['user']['name'],
       email: params['user']['email'],
       user_type: params['user']['user_type'],
       password: params['user']['password'],
       password_confirmation: params['user']['password_confirmation']
     )
-    if user
+    if user.save
       session[:user_id] = user.id
       render json: {
         status: :created,
---
+++ spec/controllers/registrations_controller_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+RSpec.describe RegistrationsController, type: :controller do
+
+  describe 'POST /registrations' do
+    before do
+      post :create, params: {
+        user: {
+          name: 'kender 02',
+          email: 'mail@mail.com',
+          password: '123456',
+          password_confirmation: '123456',
+          user_type: 'user'
+        }
+      }
+    end
+
+    context 'when user is created' do
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+  end
+
+  describe 'POST /registrations' do
+    before do
+      post :create, params: {
+        user: {
+          name: '',
+          email: 'mailmail.com',
+          password: '123456',
+          password_confirmation: '123456',
+          user_type: 'user'
+        }
+      }
+    end
+
+    context 'when user is created incorrectly' do
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+  end
+end
